661380a347ebe7c7b8e8bf0ca8b2f03f63fa5dfc
arbinCH <arbinch345@gmail.com>
2025-08-19 22:06:02 +0545

    Add files via upload

---
+++ mandlibs_game.py
@@ -0,0 +1,15 @@
+# Mandlibs game
+# This is a simple game where the player fills in the blanks of a story.
+
+noun1 = input("Enter a noun(person/character): ")
+noun2 = input("Enter another noun(place): ")
+noun3 = input("Enter a third noun(object/thing): ")
+noun4 = input("Enter a fourth noun(abstract concept): ")
+proper_noun1 = input("Enter a proper noun(stupa/temple): ")
+plural_noun1 = input("Enter a plural noun(animals/birds): ")
+adjective1 = input("Enter an adjective(description): ")
+
+print(f"In kathmandu, a young {noun1} walked through the busy streets of {noun2}")
+print(f"They found a hidden {noun3} near the old temple.")
+print(f"Following a {noun4}, they reached {proper_noun1}, where they found {plural_noun1}.")
+print(f"From that day, they knew kathmandu was full of {adjective1} stories.")
